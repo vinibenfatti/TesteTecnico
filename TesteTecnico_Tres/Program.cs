98c39fa63afd7ec0008dcd06d8fdb7c75bbf047d
Vinicius dos Anjos Benfatti <vinibenfatti@gmail.com>
2021-02-26 16:04:57 -0300

    1.0

--- TesteTecnico_Tres/Program.cs
+++ TesteTecnico_Tres/Program.cs
@@ -12,7 +12,7 @@ namespace TesteTecnico_Dois
             InvestimentoComIR investimentoComIR = new InvestimentoComIR(valorInicialUser, 0.35);
             InvestimentoSemIR investimentoSemIR = new InvestimentoSemIR(valorInicialUser, 0.3);
 
-            for (int i = 1; i < 36; i++) { 
+            for (int i = 1; i <= 36; i++) { 
        
                 double lucroIR = Math.Round(investimentoComIR.CalcularLucro(i), 2);
                 double lucroSemIR = Math.Round(investimentoSemIR.CalcularLucro(i), 2);
